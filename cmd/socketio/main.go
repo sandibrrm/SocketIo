df2fc55f575e15adc6d3c619bbc9585076c304ff
Sandi <43517564+sandibrrm@users.noreply.github.com>
2025-08-07 02:31:11 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/socketio/main.go
@@ -0,0 +1,23 @@
+// cmd/socketio/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "socketio/internal/socketio"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := socketio.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
